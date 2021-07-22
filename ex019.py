d07b9691c76c3922a556cc83686a4c6f9d045ed2
Talita Silva <77306310+talitas9@users.noreply.github.com>
2021-07-21 21:00:21 -0300

    Escolhendo um aluno.
    Um professor quer sortear um dos seus quatro alunos para apagar o quadro. 
    Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido.

--- ex019.py
+++ ex019.py
@@ -1,6 +1,3 @@
-# um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele,
-#lendo o nome deles e escrevendo o nome escolhido.
-
 from random import choice
 a1 = str(input("Digite o nome do primeiro aluno: "))
 a2 = str(input("Digite o nome do segundo aluno: "))
