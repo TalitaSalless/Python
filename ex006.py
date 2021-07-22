dd7016f1d2c7aed737c42dd84d7a09cb8400b5bb
Talita Silva <77306310+talitas9@users.noreply.github.com>
2021-07-21 21:06:56 -0300

    Raiz Quadrada.
    Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.

--- ex006.py
+++ ex006.py
@@ -1,3 +1,2 @@
-#06 Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 n = int(input('Digite um número:'))
 print (f' O dobro de {n} vale {n*2} o triplo de {n} vale {n*3} a raiz quadrada de {n} é: {n**(1/2)}')
