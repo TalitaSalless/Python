2546e80434116d0878d2798ee68bad7f43207fce
Talita Silva <77306310+talitas9@users.noreply.github.com>
2021-07-22 19:26:53 -0300

    Add files via upload

---
+++ ex023.py
@@ -0,0 +1,14 @@
+n = int(input('Digite um numero: '))
+print('Analisando o numero {}...'.format(n))
+u = n // 1 % 10
+d = n // 10 % 10
+c = n // 100 % 10
+m = n // 1000 % 10
+print('A unidade dele é: {}'.format(u))
+print('A dezena dele é: {}'.format(d))
+print('A centena dele é: {}'.format(c))
+print('O milhar dele é: {}'.format(m))
+
+
+
+
