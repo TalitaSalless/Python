b6fa36ba5850303e19851bee53e411fd0eb460bd
Talita Silva <77306310+talitas9@users.noreply.github.com>
2021-07-21 21:12:05 -0300

    Porcentagem.
    Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.

--- ex013.py
+++ ex013.py
@@ -1,5 +1,3 @@
-# 13 Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
-
 salario = float(input('Salário do funcionário: R$ '))
 aumento = salario + (salario * 15 / 100)
 print(f'O salário do funcionário com aumento de 15% será R$ {aumento} reais.')
