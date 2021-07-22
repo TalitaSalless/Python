75e80d757d20250529a1a0ee5872319f5d583cd0
Talita Silva <77306310+talitas9@users.noreply.github.com>
2021-07-21 21:10:56 -0300

    Area de pintura por m2.
    Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área de tinta necessária para pinta-la, sabendo que cada litro de tinta pinta uma área de 2m2.

--- ex011.py
+++ ex011.py
@@ -1,6 +1,3 @@
-# 11 Faça um programa que leia a largura e a altura de uma parede em metros,
-# calcule a sua área de tinta necessária para
-# pinta-la, sabendo que cada litro de tinta pinta uma área de 2m2.
 largura = float(input('Qual a largura da parede? '))
 altura = float(input('Qual a altura da parede? '))
 area = largura * altura
