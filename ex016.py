ac4509a5afbb02e320141736e25adaab8c7c061b
Talita Silva <77306310+talitas9@users.noreply.github.com>
2021-07-21 21:14:32 -0300

    Parte inteira de um número.
    Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
    Digite um numero: 6.127. O numero 6.127 tema a parte inteira 6.

--- ex016.py
+++ ex016.py
@@ -1,5 +1,3 @@
-# Crie um programa que leia um numero real qualquer pelo teclado e mostre na tela a sua porção inteira.
-# Digite um numero: 6.127. O numero 6.127 tema a parte inteira 6.
 from math import trunc
 n = float(input("Digite um numero: "))
 int = trunc(n)
