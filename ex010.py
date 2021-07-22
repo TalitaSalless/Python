f7d378ecc01e512f93028fa59ceaa4c153988b8c
Talita Silva <77306310+talitas9@users.noreply.github.com>
2021-07-21 21:10:14 -0300

    Conversão de moeda.
    Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares, euro, pesos, ela pode comprar.

--- ex010.py
+++ ex010.py
@@ -1,4 +1,3 @@
-# 10 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 carteira = float(input('Digite o valor que voce tem na carteira: R$ '))
 dolar = carteira / 4.91
 euro = carteira / 5.86
