1ea7271b82fda600cecb171f756b4c9268d23cd4
Talita Silva <77306310+talitas9@users.noreply.github.com>
2021-07-22 18:36:30 -0300

    Analisando Texto
    Crie um programa que leia o nome completo de uma pessoa e mostre: 
    - O nome com todas as letras maiúsculas e minúsculas.
    - Quantas letras ao todo (sem considerar espaços).
    - Quantas letras tem o primeiro nome.

--- ex022.py
+++ ex022.py
@@ -1,6 +1,3 @@
-#Crie um programa que leia o nome completo de uma pessoa e mostre:#O nome com todas as letras maiusculas;
-#O nome com todas as minusculas;#Quantas letras aotodo sem considerar espaços;#Quantas letras temm o primeiro nome;
-
 nome = str(input("Digite seu nome completo:")).strip()
 print("Analisando o seu nome...")
 print("Seu nome em maiúsculas é {}".format(nome.upper()))
@@ -8,5 +5,3 @@ print("Seu nome em minúsculas é {}".format(nome.lower()))
 print("Seu nome tem ao todo {} letras".format(len(nome) - nome.count(' ')))
 separa = nome.split()
 print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
-
-
