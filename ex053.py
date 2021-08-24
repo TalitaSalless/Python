1050aa73f532ce9b4a9414f516518fb64a5df834
Talita Silva <77306310+talitas9@users.noreply.github.com>
2021-08-24 19:48:37 -0300

    Delete ex053.py

--- ex053.py
+++
@@ -1,9 +0,0 @@
-frase = str(input('Digite uma frase: ')).strip().upper()
-palavras = frase.split()
-junto = ''.join(palavras)
-inverso = junto[::-1]
-print('A frase {} invertida fica {}'.format(junto, inverso))
-if inverso == junto:
-    print('A frase é um palíndromo!')
-else:
-    print('A frase não é um palíndromo!')
