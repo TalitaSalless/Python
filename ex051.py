510d1bd0e64f99afe8688d4f07794f4fec7c69a8
Talita Silva <77306310+talitas9@users.noreply.github.com>
2021-08-24 19:47:00 -0300

    Progressão Aritmética
    Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.

---
+++ ex051.py
@@ -0,0 +1,9 @@
+print('='*30)
+print('    10 TERMOS DE UMA PA')
+print('='*30)
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro,decimo + razao, razao):
+    print('{}'.format(c), end=' - ')
+print('Acabou...')
