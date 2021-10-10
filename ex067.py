ca6ce05bbfeeca1c5a2f4c19d949a7ceda35409e
Talita Silva <77306310+talitas9@users.noreply.github.com>
2021-10-09 21:32:42 -0300

    Tabuada v3.0
    Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.

---
+++ ex067.py
@@ -0,0 +1,7 @@
+while True:
+    n = int(input('Quer ver a tabuada de qual valor?'))
+    if n < 0:
+        print('Formato inválido! Encerrando o programa!')
+        break
+    for c in range(1, 11):
+        print(f' {n} x {c} = {n*c}')
